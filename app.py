44679bec9ab579fcedeb2e2b441aec38eaeba09c
Venkata Ranga Sai Krishna Parcha <158783578+venkatarangasaikrishna@users.noreply.github.com>
2025-04-26 23:29:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ def churn_view():
 
 # ─── 4) Run app ────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
